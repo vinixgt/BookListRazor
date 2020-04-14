0a5bcf7f723152424724846d035dbf78c1184f56
vinix <rojas.gt@gmail.com>
2020-04-14 17:41:03 -0600

    Add - delete book

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -26,5 +26,17 @@ namespace BookListRazor.Pages.BookList
         {
             Books = await _db.Book.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if( book == null )
+            {
+                return NotFound();
+            }
+            _db.Remove(book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
     }
 }

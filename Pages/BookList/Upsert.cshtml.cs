afb71894e8a01cd524dd1be97a0cc4e5091c1abb
vinix <rojas.gt@gmail.com>
2020-04-16 17:25:00 -0600

    Add upsert functionality

---
+++ Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+using BookListRazor.Model;
+
+namespace BookListoRazor.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if(id == null) 
+            {
+                // Create
+                return Page();
+            }
+            // for update
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.id == id);
+            if(Book == null) 
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                if (Book.id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else 
+                {
+                    _db.Book.Update(Book);
+                }
+            }
+
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
--- wwwroot/js/bookList.js
+++ wwwroot/js/bookList.js
@@ -20,7 +20,7 @@ function loadDataTable() {
                 "render": function(data) {
                     return `
                         <div class="text-center">
-                            <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer width:70px">Edit</a>
+                            <a href="/BookList/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer width:70px">Edit</a>
                             &nbsp;
                             <a  class='btn btn-danger text-white' style='cursor:pointer width:70px' onclick=Delete('/api/book?id='+${data})>Delete</a>
                         </div>

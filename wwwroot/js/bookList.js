3bdec012dad6680107da50bc1de84be56bd0d7f0
vinix <rojas.gt@gmail.com>
2020-04-16 16:47:29 -0600

    Add delete book by ajax and api/book/delete

--- wwwroot/js/bookList.js
+++ wwwroot/js/bookList.js
@@ -20,13 +20,13 @@ function loadDataTable() {
                 "render": function(data) {
                     return `
                         <div class="text-center">
-                            <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer witdh:100px">Edit</a>
+                            <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer width:70px">Edit</a>
                             &nbsp;
-                            <a  class="btn btn-danger text-white" style="cursor:pointer witdh:100px">Delete</a>
+                            <a  class='btn btn-danger text-white' style='cursor:pointer width:70px' onclick=Delete('/api/book?id='+${data})>Delete</a>
                         </div>
                     `;
                 },
-                "witdh": "30%"
+                "witdh": "40%"
             }
         ],
         "language": {
@@ -35,4 +35,31 @@ function loadDataTable() {
         },
         "width": "100%"
     });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are your sure?",
+        text: "Once deleted, you will not be able to recover.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    })
+    .then((willDelete) => {
+        if(willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function(data) {
+                    if(data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.console.error(data.message);
+                        
+                    }
+                }
+            });
+        }
+    })
 }
